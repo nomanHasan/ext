2481c683c96c2573004cf321433f6a59dca58bf6
Noman Hasan <nomanbinhussein@gmail.com>
2020-05-19 23:50:08 +0600

    Release Tue 19 May 2020 11:50:08 PM +06

---
+++ precache-manifest.8c50420e7b10b0d9e5263753b8dd7a6a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28f55018d42878ac5bc61f268bcad08",
+    "url": "/ext/alert00.mp3"
+  },
+  {
+    "revision": "31d38099a17a0810e841318c9ed9c729",
+    "url": "/ext/alert01.mp3"
+  },
+  {
+    "revision": "59582be6c18ddaf14cf34c4a9a3c8655",
+    "url": "/ext/alert02.mp3"
+  },
+  {
+    "revision": "95d88e3a0f25f266644c194ebd96f8aa",
+    "url": "/ext/alert03.mp3"
+  },
+  {
+    "revision": "984e81b2afe6bfbbaed87c1348ba2875",
+    "url": "/ext/alert04.mp3"
+  },
+  {
+    "revision": "790d9c775a6d49ba82f2642a53300c35",
+    "url": "/ext/alert05.mp3"
+  },
+  {
+    "revision": "f0ac1bf62893e682c64e564bf1af7131",
+    "url": "/ext/alert06.mp3"
+  },
+  {
+    "revision": "d5368d6e089ee9b1fb70",
+    "url": "/ext/css/app.1736daa6.css"
+  },
+  {
+    "revision": "981243c94b8903231614",
+    "url": "/ext/css/chunk-vendors.10dc1890.css"
+  },
+  {
+    "revision": "accbaa29971de74bf65731bfc54dfcda",
+    "url": "/ext/favicon.png"
+  },
+  {
+    "revision": "18152dfcd911da505410fe07c10d62fb",
+    "url": "/ext/index.html"
+  },
+  {
+    "revision": "d6cb772043fadd95b2a0",
+    "url": "/ext/js/about.b1946dad.js"
+  },
+  {
+    "revision": "d5368d6e089ee9b1fb70",
+    "url": "/ext/js/app.d7c738e6.js"
+  },
+  {
+    "revision": "981243c94b8903231614",
+    "url": "/ext/js/chunk-vendors.063b8cab.js"
+  },
+  {
+    "revision": "43018d2ca80b56ac79ea0f0abf886cb7",
+    "url": "/ext/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ext/robots.txt"
+  },
+  {
+    "revision": "cf62b6190e160c63df41227f43de2b7a",
+    "url": "/ext/service-worker.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,157 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts('./workbox/workbox-sw.js');
+// workbox.setConfig({
+//   modulePathPrefix: '/workbox/'
+// });
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/ext/precache-manifest.a43fa3c01db3f3ae97a477130aa19262.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "exercise-timer"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+workbox.setConfig({
+  modulePathPrefix: './workbox/'
 });
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "alert00.mp3",
+    "revision": "c28f55018d42878ac5bc61f268bcad08"
+  },
+  {
+    "url": "alert01.mp3",
+    "revision": "31d38099a17a0810e841318c9ed9c729"
+  },
+  {
+    "url": "alert02.mp3",
+    "revision": "59582be6c18ddaf14cf34c4a9a3c8655"
+  },
+  {
+    "url": "alert03.mp3",
+    "revision": "95d88e3a0f25f266644c194ebd96f8aa"
+  },
+  {
+    "url": "alert04.mp3",
+    "revision": "984e81b2afe6bfbbaed87c1348ba2875"
+  },
+  {
+    "url": "alert05.mp3",
+    "revision": "790d9c775a6d49ba82f2642a53300c35"
+  },
+  {
+    "url": "alert06.mp3",
+    "revision": "f0ac1bf62893e682c64e564bf1af7131"
+  },
+  {
+    "url": "css/app.1736daa6.css",
+    "revision": "bc40458a0677a90afbda8f34e7071434"
+  },
+  {
+    "url": "css/chunk-vendors.10dc1890.css",
+    "revision": "6276879aa1ad1d0116bbb0a1a35d0c9b"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "23d38844317407b9e8c235d88e910a63"
+  },
+  {
+    "url": "favicon.png",
+    "revision": "accbaa29971de74bf65731bfc54dfcda"
+  },
+  {
+    "url": "img/icons/android-chrome-144x144.png",
+    "revision": "406d9e78df9dab74e880a8abef10406e"
+  },
+  {
+    "url": "img/icons/android-chrome-192x192.png",
+    "revision": "25405677b6f3f1873658eb397d6aad07"
+  },
+  {
+    "url": "img/icons/android-chrome-512x512.png",
+    "revision": "993fa191b8aacb18fa3306dde2b84c9a"
+  },
+  {
+    "url": "img/icons/android-chrome-96x96.png",
+    "revision": "9678e5ed5e892a46c923b3e849fa293c"
+  },
+  {
+    "url": "img/icons/apple-touch-icon.png",
+    "revision": "117abacac8264c13639719870b690b43"
+  },
+  {
+    "url": "img/icons/extimer.svg",
+    "revision": "d557f2c26480229f73135b54a0a76f68"
+  },
+  {
+    "url": "img/icons/favicon_io/android-chrome-192x192.png",
+    "revision": "25405677b6f3f1873658eb397d6aad07"
+  },
+  {
+    "url": "img/icons/favicon_io/android-chrome-512x512.png",
+    "revision": "993fa191b8aacb18fa3306dde2b84c9a"
+  },
+  {
+    "url": "img/icons/favicon_io/apple-touch-icon.png",
+    "revision": "117abacac8264c13639719870b690b43"
+  },
+  {
+    "url": "img/icons/favicon_io/favicon-16x16.png",
+    "revision": "462fc482cfb26997e1ab3c52b380b010"
+  },
+  {
+    "url": "img/icons/favicon_io/favicon-32x32.png",
+    "revision": "517c717efab90fe77781f363a20aecc9"
+  },
+  {
+    "url": "img/icons/favicon_io/favicon.ico",
+    "revision": "f0343a47f352dd1732d1dc3e541051ce"
+  },
+  {
+    "url": "img/icons/favicon_io/site.webmanifest",
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e"
+  },
+  {
+    "url": "img/icons/favicon-16x16.png",
+    "revision": "462fc482cfb26997e1ab3c52b380b010"
+  },
+  {
+    "url": "img/icons/favicon-32x32.png",
+    "revision": "517c717efab90fe77781f363a20aecc9"
+  },
+  {
+    "url": "img/icons/favicon.ico",
+    "revision": "f0343a47f352dd1732d1dc3e541051ce"
+  },
+  {
+    "url": "img/icons/safari-pinned-tab.svg",
+    "revision": "d557f2c26480229f73135b54a0a76f68"
+  },
+  {
+    "url": "index.html",
+    "revision": "18152dfcd911da505410fe07c10d62fb"
+  },
+  {
+    "url": "js/about.b1946dad.js",
+    "revision": "a7016d0e5b85d5fb3250a3a7a92b3b23"
+  },
+  {
+    "url": "js/app.d7c738e6.js",
+    "revision": "b3a13b29e165076f0397cdb9f988f283"
+  },
+  {
+    "url": "js/chunk-vendors.063b8cab.js",
+    "revision": "1fe2e186d53d15132476e1fe141f7735"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "43018d2ca80b56ac79ea0f0abf886cb7"
+  },
+  {
+    "url": "precache-manifest.8c50420e7b10b0d9e5263753b8dd7a6a.js",
+    "revision": "8c50420e7b10b0d9e5263753b8dd7a6a"
+  },
+  {
+    "url": "robots.txt",
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca"
+  }
+]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
